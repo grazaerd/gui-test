7b7551ee1fca41d8c324fe8cb484b0a090f284a9
grazaerd <dnblank143@gmail.com>
2025-08-01 05:09:21 +0800

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,39 @@
-#include "FL/Fl.h"
-#include "FL/Fl_Window.h"
-#include "FL/Fl_Button.h"
+#include <windows.h>
+#include <future>
+#include <cstdio>
+#include "FL/Fl.H"
+#include "FL/Fl_Window.H"
+#include "FL/Fl_Button.H"
 
-void on_button_click(Fl_Widget*, void*) {
-    printf("Button clicked!\n");
+std::future<void> bg_task;
+Fl_Button* button = nullptr;
+
+void enable_cb(void* userdata) {
+    Fl_Button* btn = static_cast<Fl_Button*>(userdata);
+    btn->activate();
+    printf("Button reactivated\n");
+}
+
+void on_button_click(Fl_Widget*) {
+    if (bg_task.valid() && bg_task.wait_for(std::chrono::seconds(0)) != std::future_status::ready)
+        return;
+
+    button->deactivate();
+    printf("Task started\n");
+
+    bg_task = std::async(std::launch::async, []() {
+        Sleep(2400); 
+        Fl::awake(enable_cb, button);
+    });
 }
 
 int main(int argc, char** argv) {
-    Fl_Window* window = new Fl_Window(340, 180, "FLTK Example");
-    Fl_Button* button = new Fl_Button(110, 70, 120, 40, "Click Me");
+    Fl::lock(); // for multi thread
+    Fl_Window* win = new Fl_Window(340, 180, "FLTK Async Button");
+    button = new Fl_Button(110, 70, 120, 40, "Click Me");
     button->callback(on_button_click);
-    window->end();
-    window->show(argc, argv);
+    win->end();
+    win->show(argc, argv);
+
     return Fl::run();
 }
